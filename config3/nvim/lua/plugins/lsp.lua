7642fad39d40b60130dcb123a5d339051b175c8b
Ashish Ahuja <shivashish.ahuja@gmail.com>
2026-01-16 13:05:16 +0530

    get rid of pyright

--- config3/nvim/lua/plugins/lsp.lua
+++ config3/nvim/lua/plugins/lsp.lua
@@ -116,7 +116,7 @@ return {
 				},
 			},
 			-- gopls = {},
-			pyright = {},
+			--pyright = {},
 			-- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
